5d1118320edbb5dd1862d199e6c6ce6bded673b8
Han Bao <hbao@wpi.edu>
2019-08-14 23:56:32 -0400

    add file interface implementation

--- src/main/java/com/bh/news/server/util/file/FileHelper.java
+++ src/main/java/com/bh/news/server/util/file/FileHelper.java
@@ -1,4 +1,17 @@
 package com.bh.news.server.util.file;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface FileHelper {
+    @Value("${storage.directory}")
+    String SAVE_PATH = null;
+
+    boolean saveFile(String path, String fileName);
+    boolean deleteFile(String path, String fileName);
+    boolean updateFile(String path, String fileName);
+    boolean renameFile(String path, String oldFileName, String newFileName);
+
+    boolean createFolder(String path);
+    boolean deleteFolder(String path);
+    boolean renameFolder(String path);
 }
---
+++ src/main/java/com/bh/news/server/util/file/FileHelperIo.java
@@ -0,0 +1,39 @@
+package com.bh.news.server.util.file;
+
+public class FileHelperIo implements FileHelper{
+
+    @Override
+    public boolean saveFile(String path, String fileName) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteFile(String path, String fileName) {
+        return false;
+    }
+
+    @Override
+    public boolean updateFile(String path, String fileName) {
+        return false;
+    }
+
+    @Override
+    public boolean renameFile(String path, String oldFileName, String newFileName) {
+        return false;
+    }
+
+    @Override
+    public boolean createFolder(String path) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteFolder(String path) {
+        return false;
+    }
+
+    @Override
+    public boolean renameFolder(String path) {
+        return false;
+    }
+}
